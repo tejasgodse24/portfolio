1d8cd9013869ebd032ccc0a036adf221cae561cb
tejas24 <tejasgodse19@gmail.com>
2025-04-26 19:08:11 +0530

    added bold report

--- src/constants/index.js
+++ src/constants/index.js
@@ -12,7 +12,7 @@ export const HERO_CONTENT =  "Experienced software developer skilled in Fullstac
 
 
 
-export const ABOUT_TEXT = `I am a passionate full stack developer specialised in crafting seamless software applications. With more than 1 years of hands-on experience, Some skills on which i have worked through my different projects are Backend Development (Django, Django Rest Framework, Python, SQL Server, Celery, ASP.NET, .NET Web API), Frontend development (ReactJS, HTML, CSS, JavaScript, JQuery), Deployment(AWS EC2, Hostinger's VPS, Nginx, Github Actions, AWS S3) and others are  OOP , Data Structures and Algorithms (C++). I am Committed to continuous learning and staying up-to-date with emerging technologies. Eager to contribute my skills and passion to a dynamic team and drive innovative projects.`;
+export const ABOUT_TEXT = `I am a passionate full stack developer specialised in crafting seamless software applications. With almost 2 years of hands-on experience, Some skills on which i have worked through my different projects are Backend Development (Django, Django Rest Framework, Python, SQL Server, Celery, ASP.NET, .NET Web API), Frontend development (ReactJS, HTML, CSS, JavaScript, JQuery), Deployment(AWS EC2, Hostinger's VPS, Nginx, Github Actions, AWS S3) and others are  OOP , Data Structures and Algorithms (C++). I am Committed to continuous learning and staying up-to-date with emerging technologies. Eager to contribute my skills and passion to a dynamic team and drive innovative projects.`;
 
 
 export const EXPERIENCES = [
@@ -20,7 +20,7 @@ export const EXPERIENCES = [
     year: "May 2024 - Present",
     role: "Software Developer",
     company: "Cloud9 e-biz Pvt Ltd",
-    description: `Designed and implemented a Single Sign-On (SSO) system from scratch, integrating it with multiple systems.*Maintained and Integrated backend solutions for mobile and web platforms which includes custom Service Desk SaaS application, enhancing system efficiency and improving user response times by 30%. 
+    description: `Designed and implemented a Single Sign-On (SSO) system from scratch, integrating it with multiple systems.*Integrated Bold Reports Embedding Service into ASP.NET MVC application, enabling dynamic report rendering.*Maintained and Integrated backend solutions for mobile and web platforms which includes custom Service Desk SaaS application, enhancing system efficiency and improving user response times by 30%. 
 *Built robust APIs and numerous complex stored procedures, reducing data retrieval times by up to 25%.  
 *Enabled efficient migration of datasets across multiple databases by writing SQL scripts and stored procedures, 
 ensuring data integrity and reducing manual data verification.  
